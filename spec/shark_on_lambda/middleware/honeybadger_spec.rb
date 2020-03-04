1af83a6d2ec09820dcec728604d530aaebed1f73
Huy Dinh <mail@huydinh.eu>
2020-03-04 17:21:17 +0100

    Refactor `Middleware::Honeybadger` tests.

--- spec/shark_on_lambda/middleware/honeybadger_spec.rb
+++ spec/shark_on_lambda/middleware/honeybadger_spec.rb
@@ -1,5 +1,50 @@
 # frozen_string_literal: true
 
+RSpec.shared_examples 'does not trigger a Honeybadger notification' do
+  subject(:response) { instance.call(env) }
+
+  it 'does not trigger a Honeybadger notification' do
+    expect(Honeybadger).to_not receive(:notify)
+
+    if defined?(app_exception)
+      expect { response }.to raise_error(app_exception)
+    else
+      response
+    end
+  end
+end
+
+RSpec.shared_examples 'triggers a Honeybadger notification' do
+  subject(:response) { instance.call(env) }
+
+  it 'triggers a Honeybadger notification' do
+    expect(Honeybadger).to receive(:notify).with(app_exception, anything)
+    expect { response }.to raise_error(app_exception)
+  end
+
+  it 'includes additional information in the Honeybadger notification' do
+    expected_options = {
+      tags: tags,
+      controller: env['shark.controller'],
+      action: env['shark.action'],
+      parameters: env['action_dispatch.request.parameters']
+    }
+
+    expect(Honeybadger).to(
+      receive(:notify).with(app_exception, hash_including(expected_options))
+    )
+    expect { response }.to raise_error(app_exception)
+  end
+end
+
+RSpec.shared_examples 'raises the exception from the app' do
+  subject(:response) { instance.call(env) }
+
+  it 'raises the exception from the app' do
+    expect { response }.to raise_error(app_exception)
+  end
+end
+
 RSpec.describe SharkOnLambda::Middleware::Honeybadger do
   let!(:env) do
     {
@@ -23,15 +68,15 @@ RSpec.describe SharkOnLambda::Middleware::Honeybadger do
     Object.send(:remove_const, :Honeybadger)
   end
 
-  subject(:response) { instance.call(env) }
-
   context 'with no exceptions from the app' do
     let!(:app_response) { [200, {}, ['Hello, world!']] }
     let!(:app) { ->(_env) { app_response } }
 
     it 'returns the response from the app' do
-      expect(response).to eq(app_response)
+      expect(instance.call(env)).to eq(app_response)
     end
+
+    include_examples 'does not trigger a Honeybadger notification'
   end
 
   context 'with a SharkOnLambda exception from the app' do
@@ -41,14 +86,8 @@ RSpec.describe SharkOnLambda::Middleware::Honeybadger do
       end
       let!(:app) { ->(_env) { raise app_exception } }
 
-      it 'raises the exception from the app' do
-        expect { response }.to raise_error(app_exception)
-      end
-
-      it 'does not trigger a Honeybadger notification' do
-        expect(Honeybadger).to_not receive(:notify)
-        expect { response }.to raise_error(app_exception)
-      end
+      include_examples 'raises the exception from the app'
+      include_examples 'does not trigger a Honeybadger notification'
     end
 
     context 'with a server error' do
@@ -57,28 +96,8 @@ RSpec.describe SharkOnLambda::Middleware::Honeybadger do
       end
       let!(:app) { ->(_env) { raise app_exception } }
 
-      it 'raises the exception from the app' do
-        expect { response }.to raise_error(app_exception)
-      end
-
-      it 'triggers a Honeybadger notification' do
-        expect(Honeybadger).to receive(:notify).with(app_exception, anything)
-        expect { response }.to raise_error(app_exception)
-      end
-
-      it 'includes additional information in the Honeybadger notification' do
-        expected_options = {
-          tags: tags,
-          controller: env['shark.controller'],
-          action: env['shark.action'],
-          parameters: env['action_dispatch.request.parameters']
-        }
-
-        expect(Honeybadger).to(
-          receive(:notify).with(app_exception, hash_including(expected_options))
-        )
-        expect { response }.to raise_error(app_exception)
-      end
+      include_examples 'raises the exception from the app'
+      include_examples 'triggers a Honeybadger notification'
     end
   end
 
@@ -86,27 +105,7 @@ RSpec.describe SharkOnLambda::Middleware::Honeybadger do
     let!(:app_exception) { RuntimeError.new('Nothing to see here.') }
     let!(:app) { ->(_env) { raise app_exception } }
 
-    it 'raises the exception from the app' do
-      expect { response }.to raise_error(app_exception)
-    end
-
-    it 'triggers a Honeybadger notification' do
-      expect(Honeybadger).to receive(:notify).with(app_exception, anything)
-      expect { response }.to raise_error(app_exception)
-    end
-
-    it 'includes additional information in the Honeybadger notification' do
-      expected_options = {
-        tags: tags,
-        controller: env['shark.controller'],
-        action: env['shark.action'],
-        parameters: env['action_dispatch.request.parameters']
-      }
-
-      expect(Honeybadger).to(
-        receive(:notify).with(app_exception, hash_including(expected_options))
-      )
-      expect { response }.to raise_error(app_exception)
-    end
+    include_examples 'raises the exception from the app'
+    include_examples 'triggers a Honeybadger notification'
   end
 end
