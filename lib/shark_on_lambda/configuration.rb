59519d59a4551ec719589c2e1879912a74f17052
Huy Dinh <mail@huydinh.eu>
2020-09-19 21:12:41 +0200

    Rework `Application` to match `Rails::Application` more closely.

--- lib/shark_on_lambda/configuration.rb
+++ lib/shark_on_lambda/configuration.rb
@@ -2,70 +2,18 @@
 
 module SharkOnLambda
   class Configuration < OpenStruct
-    include Concerns::ResettableSingleton
+    include Singleton
 
-    attr_writer :stage
+    attr_reader :root
 
-    class << self
-      include Concerns::YamlConfigLoader
-
-      attr_writer :database_files, :settings_files
-
-      def database_files
-        return @database_files if defined?(@database_files)
-
-        files = %w[config/database.yml config/database.local.yml]
-        @database_files = paths(files)
+    def middleware
+      @middleware ||= ActionDispatch::MiddlewareStack.new do |middleware_stack|
+        middleware_stack.use Middleware::LambdaLogger
       end
-
-      def load(stage, fallback: :default)
-        load_settings(stage, fallback: fallback)
-        load_database_configuration(stage, fallback: fallback)
-
-        instance
-      end
-
-      def settings_files
-        return @settings_files if defined?(@settings_files)
-
-        files = %w[config/settings.yml config/settings.local.yml]
-        @settings_files = paths(files)
-      end
-
-      protected
-
-      def load_database_configuration(stage, fallback:)
-        instance.database = load_yaml_files(stage: stage,
-                                            fallback: fallback,
-                                            paths: paths(database_files))
-      end
-
-      def load_settings(stage, fallback:)
-        settings = load_yaml_files(stage: stage,
-                                   fallback: fallback,
-                                   paths: paths(settings_files))
-        settings.each_pair do |key, value|
-          next if key.to_s == 'serverless'
-
-          instance.send("#{key}=", value)
-        end
-      end
-
-      def paths(files)
-        files.map { |file| SharkOnLambda.config.root.join(file) }
-      end
-    end
-
-    def root
-      @root ||= Pathname.new('.')
     end
 
     def root=(new_root)
       @root = Pathname.new(new_root)
-    end
-
-    def stage
-      @stage || 'development'
     end
   end
 end
--- spec/test_application/config/routes.rb
+++ spec/test_application/config/routes.rb
@@ -2,4 +2,7 @@
 
 SharkOnLambda.application.routes.draw do
   get '/api_gateway', to: 'test_application/api_gateway#index'
+  match '/api_gateway', via: :all,
+                        to: 'test_application/api_gateway#some_action'
+  match '/api_gateway', via: :all, to: 'test_application/api_gateway#foo'
 end
