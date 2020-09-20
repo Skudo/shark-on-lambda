3e93466949b64b8e43b69c13a7003ed33f117b3c
Huy Dinh <mail@huydinh.eu>
2020-09-20 15:31:55 +0200

    Provide URL helpers in `RSpec::Helpers`.

--- lib/shark_on_lambda/rspec/helpers.rb
+++ lib/shark_on_lambda/rspec/helpers.rb
@@ -3,8 +3,13 @@
 module SharkOnLambda
   module RSpec
     module Helpers
+      extend ActiveSupport::Concern
       include RequestHelpers
       include ResponseHelpers
+
+      included do
+        include SharkOnLambda.application.routes.url_helpers
+      end
     end
   end
 end
